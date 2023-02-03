201ce0a63bfc0421432da6e6a32ed4a43d7e23fd
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-03 12:35:09 -0300

    fix: resolving bug when creating a new task

--- src/main/java/com/ws/taskmanager/data/DTO/TaskCreateDTO.java
+++ src/main/java/com/ws/taskmanager/data/DTO/TaskCreateDTO.java
@@ -1,37 +1,19 @@
 package com.ws.taskmanager.data.DTO;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.github.dozermapper.core.Mapping;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-@JsonPropertyOrder({"id", "task", "concluded", "deadline", "createdAt"})
-public class TaskCreateDTO extends RepresentationModel<TaskResponseDTO> implements Serializable {
-
-    @JsonProperty("id")
-    @Mapping("id")
-    private UUID key;
-
+public class TaskCreateDTO extends RepresentationModel<TaskCreateDTO> implements Serializable {
     @NotBlank
     private String task;
 
     @FutureOrPresent
     private LocalDateTime deadline;
 
-
-    public UUID getKey() {
-        return key;
-    }
-
-    public void setKey(UUID key) {
-        this.key = key;
-    }
 
     public String getTask() {
         return task;
@@ -51,42 +33,21 @@ public class TaskCreateDTO extends RepresentationModel<TaskResponseDTO> implemen
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
 
         TaskCreateDTO that = (TaskCreateDTO) o;
 
-        if (!getKey().equals(that.getKey())) {
-            return false;
-        }
-        if (!getTask().equals(that.getTask())) {
-            return false;
-        }
-        return getDeadline().equals(that.getDeadline());
+        if (!task.equals(that.task)) return false;
+        return deadline.equals(that.deadline);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + getKey().hashCode();
-        result = 31 * result + getTask().hashCode();
-        result = 31 * result + getDeadline().hashCode();
+        result = 31 * result + task.hashCode();
+        result = 31 * result + deadline.hashCode();
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "TaskCreateDTO{" +
-                "key=" + key +
-                ", task='" + task + '\'' +
-                ", deadline=" + deadline +
-                '}';
     }
 }
