dfb336d7e0c54d4d856ff49e5c6dddb2ba9ab155
João Paulo <jpcchaves@outlook.com>
2023-03-12 21:26:56 -0300

    feat: add jpa timezone config

--- src/main/java/com/ws/taskmanager/configs/AppConfig.java
+++ src/main/java/com/ws/taskmanager/configs/AppConfig.java
@@ -4,12 +4,13 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.TimeZone;
+import java.util.spi.TimeZoneNameProvider;
 
 @Configuration
 public class AppConfig {
 
     @PostConstruct
     public void init(){
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));   // It will set UTC timezone
+        TimeZone.setDefault(TimeZone.getTimeZone(""));   // It will set UTC timezone
     }
 }
