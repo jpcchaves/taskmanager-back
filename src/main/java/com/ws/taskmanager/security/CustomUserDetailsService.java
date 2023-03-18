b78ff5daef1267ef91f67a9ba7cf9dcc42dc83a3
João Paulo <jpcchaves@outlook.com>
2023-03-18 11:41:39 -0300

    feat: impl custom user details service

---
+++ src/main/java/com/ws/taskmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.ws.taskmanager.security;
+
+import com.ws.taskmanager.exceptions.ResourceNotFoundException;
+import com.ws.taskmanager.repositories.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        var user = userRepository
+                .findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Usuário não encontrado com os dados informados: " + usernameOrEmail)
+                );
+
+        // Convert a set of roles into a set of SimpleGrantedAuthorities
+        Set<GrantedAuthority> authorities = user
+                .getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
