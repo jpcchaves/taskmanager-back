33b1e4c66707fa854bfe4191118bcdd9c928a7c7
João Paulo <jpcchaves@outlook.com>
2023-03-17 23:41:09 -0300

    feat: add role entity and many to many relation with user entity

--- src/main/java/com/ws/taskmanager/models/UserModel.java
+++ src/main/java/com/ws/taskmanager/models/UserModel.java
@@ -2,6 +2,8 @@ package com.ws.taskmanager.models;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 public class UserModel {
@@ -20,16 +22,31 @@ public class UserModel {
 
     private String password;
 
+    // used Set to avoid duplicates roles
+    // FetchType.EAGER -> means that when user is loaded, his role will be retrieved too
+    // CascadeType.ALL -> means that when user is saved, his rle will be saved too
+    @ManyToMany(
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<RoleModel> roles;
+
     public UserModel() {
-        
+
     }
 
-    public UserModel(Long id, String name, String username, String email, String password) {
+    public UserModel(Long id, String name, String username, String email, String password, Set<RoleModel> roles) {
         this.id = id;
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.roles = roles;
     }
 
     public Long getId() {
@@ -70,5 +87,13 @@ public class UserModel {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<RoleModel> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleModel> roles) {
+        this.roles = roles;
     }
 }
