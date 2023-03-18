13eb53b50d85058293def6fea6e888e6fc0fa65a
João Paulo <jpcchaves@outlook.com>
2023-03-18 11:24:42 -0300

    feat: add role jpa repository

---
+++ src/main/java/com/ws/taskmanager/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.ws.taskmanager.repositories;
+
+import com.ws.taskmanager.models.RoleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<RoleModel, Long> {
+    Optional<RoleModel> findByName(String name);
+}
