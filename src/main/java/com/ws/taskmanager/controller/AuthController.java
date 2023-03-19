2a307ea694b10f3599574ec835e37b323c6d4075
João Paulo <jpcchaves@outlook.com>
2023-03-19 11:17:47 -0300

    feat: impl register auth controller

--- src/main/java/com/ws/taskmanager/controller/AuthController.java
+++ src/main/java/com/ws/taskmanager/controller/AuthController.java
@@ -2,7 +2,10 @@ package com.ws.taskmanager.controller;
 
 import com.ws.taskmanager.data.DTO.JWTAuthResponseDto;
 import com.ws.taskmanager.data.DTO.LoginDto;
+import com.ws.taskmanager.data.DTO.RegisterDto;
+import com.ws.taskmanager.data.DTO.RegisterResponseDto;
 import com.ws.taskmanager.services.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,5 +24,10 @@ public class AuthController {
     @PostMapping(value = {"/login", "/signin"})
     public ResponseEntity<JWTAuthResponseDto> login(@RequestBody LoginDto loginDto) {
         return ResponseEntity.ok(authService.login(loginDto));
+    }
+
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterDto registerDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerDto));
     }
 }
