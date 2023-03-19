3830bd354b960dc6e16486f71b2fbb65291ef2ae
João Paulo <jpcchaves@outlook.com>
2023-03-19 12:08:30 -0300

    feat: add custom http method not supported exception

--- src/main/java/com/ws/taskmanager/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/ws/taskmanager/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -3,11 +3,11 @@ package com.ws.taskmanager.exceptions.handler;
 import com.ws.taskmanager.exceptions.BadRequestException;
 import com.ws.taskmanager.exceptions.ResourceNotFoundException;
 import com.ws.taskmanager.exceptions.model.ExceptionResponse;
-import java.util.Objects;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Objects;
 
 @ControllerAdvice
 @RestController
@@ -42,13 +44,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
 
+    @Override
+    public final ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Metódo não suportado para o recurso: " + ex.getMethod() + ". Os metodos suportados pelo recurso são: "
+                + Arrays.toString(ex.getSupportedMethods()), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), Objects.requireNonNull(
-            ex.getFieldError()).getDefaultMessage(), request.getDescription(false));
+                ex.getFieldError()).getDefaultMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
