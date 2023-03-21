3a868179bbf512a353a576ab2ae5aa5bf60e163d
João Paulo <jpcchaves@outlook.com>
2023-03-21 12:55:15 -0300

    feat: add auth controller swagger docs

--- src/main/java/com/ws/taskmanager/controller/AuthController.java
+++ src/main/java/com/ws/taskmanager/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.ws.taskmanager.controller;
 
-import com.ws.taskmanager.data.DTO.JWTAuthResponseDto;
-import com.ws.taskmanager.data.DTO.LoginDto;
-import com.ws.taskmanager.data.DTO.RegisterDto;
-import com.ws.taskmanager.data.DTO.RegisterResponseDto;
+import com.ws.taskmanager.data.DTO.*;
 import com.ws.taskmanager.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +22,35 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Authenticates a user",
+            description = "Authenticates a user by passing the username and password",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = LoginDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PostMapping(value = {"/login", "/signin"})
     public ResponseEntity<JWTAuthResponseDto> login(@RequestBody LoginDto loginDto) {
         return ResponseEntity.ok(authService.login(loginDto));
     }
 
+    @Operation(summary = "Creates a new user",
+            description = "Creates a new user by passing in a JSON representation of the user!",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = RegisterDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     @PostMapping(value = {"/register", "/signup"})
     public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterDto registerDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerDto));
