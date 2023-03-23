0553de95876bb51635a51f37d976c89f3078926a
João Paulo <jpcchaves@outlook.com>
2023-03-22 23:06:00 -0300

    feat: add update user rest controller

--- src/main/java/com/ws/taskmanager/controller/AuthController.java
+++ src/main/java/com/ws/taskmanager/controller/AuthController.java
@@ -1,16 +1,10 @@
 package com.ws.taskmanager.controller;
 
-import com.ws.taskmanager.data.DTO.JWTAuthResponseDto;
-import com.ws.taskmanager.data.DTO.LoginDto;
-import com.ws.taskmanager.data.DTO.RegisterDto;
-import com.ws.taskmanager.data.DTO.RegisterResponseDto;
+import com.ws.taskmanager.data.DTO.*;
 import com.ws.taskmanager.services.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -29,5 +23,11 @@ public class AuthController {
     @PostMapping(value = {"/register", "/signup"})
     public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterDto registerDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerDto));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UpdateUserResponseDto> update(@RequestBody UpdateUserRequestDto updateUserDto,
+                                                        @PathVariable(name = "id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.update(updateUserDto, id));
     }
 }
