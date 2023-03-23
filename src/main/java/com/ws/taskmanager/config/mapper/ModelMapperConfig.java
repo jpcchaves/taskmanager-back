0fab649481171da496585ba6aca93103dfcd40e4
João Paulo <jpcchaves@outlook.com>
2023-03-23 11:10:52 -0300

    feat: add model mapper config

--- src/main/java/com/ws/taskmanager/config/mapper/ModelMapperConfig.java
+++ src/main/java/com/ws/taskmanager/config/mapper/ModelMapperConfig.java
@@ -1,2 +1,13 @@
-package com.ws.taskmanager.config.mapper;public class ModelMapperConfig {
+package com.ws.taskmanager.config.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
