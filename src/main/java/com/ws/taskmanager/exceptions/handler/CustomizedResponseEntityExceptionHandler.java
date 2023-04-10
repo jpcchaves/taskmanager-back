d53095096790f1537b25ba6ffa93c694af7c932b
João Paulo <jpcchaves@outlook.com>
2023-04-10 14:46:46 -0300

    feat: add handler for the MethodArgumentTypeMismatchException

--- src/main/java/com/ws/taskmanager/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/ws/taskmanager/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
@@ -38,6 +39,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public final ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Dados inválidos! Verifique os dados enviados e tente novamente!", request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BadRequestException.class)
