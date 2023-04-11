5aec7fb05046d4231477652dc8ce9e9a2383550d
João Paulo <jpcchaves@outlook.com>
2023-04-11 11:00:34 -0300

    feat: add enable caching annotation

--- src/main/java/com/ws/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/ws/taskmanager/TaskmanagerApplication.java
@@ -2,12 +2,14 @@ package com.ws.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TaskmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskmanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerApplication.class, args);
+    }
 
 }
