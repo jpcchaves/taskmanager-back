8f58e702c91957f258c09b0ad9c687923a41bdbc
João Paulo <jpcchaves@outlook.com>
2023-06-01 11:11:01 -0300

    refactor: adjusted migrations

--- src/main/resources/db/migration/V7__Insert_Roles.sql
+++ src/main/resources/db/migration/V7__Insert_Roles.sql
@@ -1,3 +1,3 @@
-INSERT INTO `railway`.`roles` (`id`, `name`)
+INSERT INTO `taskmanager`.`roles` (`id`, `name`)
 VALUES ('1', 'ROLE_ADMIN'),
        ('2', 'ROLE_USER');
--- src/main/resources/db/migration/V8__Insert_Admin_Role_In_Role_Users.sql
+++ src/main/resources/db/migration/V8__Insert_Admin_Role_In_Role_Users.sql
@@ -1,2 +1,2 @@
-INSERT INTO `railway`.`users_roles` (`user_id`, `role_id`)
+INSERT INTO `taskmanager`.`users_roles` (`user_id`, `role_id`)
 VALUES ('1', '1');
